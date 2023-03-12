03afcf71fed48898d8b68fc3fa97207d6b420970
Bent Fladmark <benteven@gmail.com>
2023-03-12 11:31:30 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,17 @@
+require('log-timestamp');
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	  console.log(req.method, req.url, JSON.stringify(req.headers))
+	  res.statusCode = 200;
+	  res.setHeader('Content-Type', 'text/plain');
+	  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+	  console.log(`Server running at http://${hostname}:${port}/`);
+	});
